a61ccc37ea853c17456a68ee674741392c26eb34
hfellerhoff <48483883+hfellerhoff@users.noreply.github.com>
2020-04-03 15:51:29 -0400

    Delete deploy_server.sh

--- deploy_server.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-yarn build:server
-docker build -t henryfellerhoff/scorecompanion:latest .
-docker push henryfellerhoff/scorecompanion:latest
-ssh root@161.35.1.70 "docker pull henryfellerhoff/scorecompanion:latest && docker tag henryfellerhoff/scorecompanion:latest dokku/api:latest && dokku tags:deploy api latest"
