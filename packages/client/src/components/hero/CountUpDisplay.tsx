a9f5a1a3dea228eaecda0d9e0cb8d3008a9dc667
Henry Fellerhoff <henryfellerhoff@gmail.com>
2020-04-02 20:52:54 -0400

    Made number count up responsive

--- packages/client/src/components/hero/CountUpDisplay.tsx
+++ packages/client/src/components/hero/CountUpDisplay.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import CountUp, { useCountUp } from 'react-countup';
+import { useCountUp } from 'react-countup';
 import { useQuery } from '@apollo/client';
 import getCounts from '../../graphql/queries/getCounts';
 import Text from 'antd/lib/typography/Text';
@@ -11,21 +11,40 @@ const addCommasToNumber = (n: number) => {
 
 const CountUpDisplay = () => {
   const { data } = useQuery(getCounts);
-  const { countUp: siteCount, update: updateSite } = useCountUp({
+  const {
+    countUp: siteCount,
+    update: updateSite,
+    reset: resetSite,
+  } = useCountUp({
     end: 0,
     formattingFn: addCommasToNumber,
+    duration: 5,
   });
-  const { countUp: workCount, update: updateWork } = useCountUp({
+  const {
+    countUp: workCount,
+    update: updateWork,
+    reset: resetWork,
+  } = useCountUp({
     end: 0,
     formattingFn: addCommasToNumber,
+    duration: 5,
   });
-  const { countUp: scoreCount, update: updateScore } = useCountUp({
+  const {
+    countUp: scoreCount,
+    update: updateScore,
+    reset: resetScore,
+  } = useCountUp({
     end: 0,
     formattingFn: addCommasToNumber,
+    duration: 5,
   });
 
   useEffect(() => {
     if (data) {
+      resetSite();
+      resetWork();
+      resetScore();
+
       updateSite(data.siteCount);
       updateWork(data.workCount);
       updateScore(data.scoreCount);
@@ -35,17 +54,21 @@ const CountUpDisplay = () => {
   return (
     <>
       <div className='count-up-display'>
-        <div className='count-up-display__block count-up-display__block--one'>
-          <Text className='count-up-display__number'>{siteCount}</Text>
-          <Text className='count-up-display__label'>Site</Text>
+        <div className='count-up-display__block-container count-up-display__block-container--top'>
+          <div className='count-up-display__block count-up-display__block--one'>
+            <Text className='count-up-display__number'>{siteCount}</Text>
+            <Text className='count-up-display__label'>Site</Text>
+          </div>
+          <div className='count-up-display__block count-up-display__block--two'>
+            <Text className='count-up-display__number'>{workCount}</Text>
+            <Text className='count-up-display__label'>Works</Text>
+          </div>
         </div>
-        <div className='count-up-display__block count-up-display__block--two'>
-          <Text className='count-up-display__number'>{workCount}</Text>
-          <Text className='count-up-display__label'>Works</Text>
-        </div>
-        <div className='count-up-display__block count-up-display__block--three'>
-          <Text className='count-up-display__number'>{scoreCount}</Text>
-          <Text className='count-up-display__label'>Scores</Text>
+        <div className='count-up-display__block-container count-up-display__block-container--bottom'>
+          <div className='count-up-display__block count-up-display__block--three'>
+            <Text className='count-up-display__number'>{scoreCount}</Text>
+            <Text className='count-up-display__label'>Scores</Text>
+          </div>
         </div>
       </div>
     </>
